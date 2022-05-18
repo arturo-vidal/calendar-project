77ae2e70a2794609fa3f5be057d008d27ac26243
arturo vidal <arturohernandezvidal.ahv@gmail.com>
2022-05-17 23:11:31 -0500

    add setNewDate and getTotalDays functions

--- calendario proyecto.js
+++ calendario proyecto.js
@@ -8,6 +8,8 @@ let currentYear= currentDate.getFullYear();
 document.getElementById("calendarMonth").textContent=months[currentMonth];
 document.getElementById("fulldate").textContent=days[currentDate.getDay()]+ " "+ currentDay+ " de "+months[currentMonth]+ " de "+currentYear;
 
+document.getElementById("prevmonth").addEventListener("click", ()=>prevmonthf());
+document.getElementById("nextmonth").addEventListener("click", ()=>nextmonthf());
 //para saber si es año bisiesto
 function leapyear(){
     if((currentYear%100!==0)&&(currentYear%4===0)||(currentYear%400===0)){
@@ -20,5 +22,55 @@ function firstday(){
     let start = new Date(currentYear, currentMonth, 1);
     return start.getDay();
 }
+//para retroceder un mes, si no es enero resta 1 mes, si es enero pasa a diciembre y resta 1 año
+function prevmonthf(){
+    if(currentMonth!==0){
+        currentMonth--;
+    }else{
+        currentMonth=11;
+        currentYear--;
+    }
+    setNewDate();
+}
+//para avanzar un mes, si no es diciembre suma 1 mes, si es diciembre pasa a enero y suma 1 año
+function nextmonthf(){
+    if(currentMonth!==11){
+        currentMonth++;
+    }else{
+        currentMonth=0;
+        currentYear++;
+    }
+    setNewDate();
+}
+//para actualizar los valores de mes y año al avanzar o retroceder de mes
+function setNewDate(){
+    currentDate.setFullYear(currentYear, currentMonth, currentDay);
+    document.getElementById("calendarMonth").textContent=months[currentMonth];
+    document.getElementById("fulldate").textContent=days[currentDate.getDay()]+ " "+ currentDay+ " de "+months[currentMonth]+ " de "+currentYear;
+}
+//para saber cuantos días tiene cada mes
+function getTotalDays(currentMonth){
+    if(currentMonth===-1){
+        currentMonth=11;
+    }
+    if(currentMonth==0||currentMonth==2||currentMonth==4||currentMonth==6||currentMonth==7||currentMonth==9||currentMonth==11){
+        return 31;
+    }else if(currentMonth==3||currentMonth==5||currentMonth==8||currentMonth==10){
+        return 30;
+    }else{
+        if(leapyear()==true){
+            return 29;
+        }else{
+            return 28;
+        }
+    }
+}
+//enumera los días del mes y los asigna al elemento div
+/*function setMonthDays(currentMonth){
+    for(let e=1; e<=getTotalDays(currentMonth); e++){
+        document.getElementById("monthdays").innerHTML+=<div class="day">${e}</div>;
+    }
+}
+setMonthDays(currentMonth);*/
 
-console.log(firstday());
+console.log(currentDate.setFullYear(currentYear, currentMonth, currentDay));
